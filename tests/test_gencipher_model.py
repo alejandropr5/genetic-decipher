8c54b7de968364b3290de470db7e8367ab162992
alejandropr05 <alejandropr05@hotmail.com>
2023-10-21 18:41:04 -0500

    Add decipher_generator test

--- tests/test_gencipher_model.py
+++ tests/test_gencipher_model.py
@@ -4,19 +4,37 @@ from gencipher.model import GeneticDecipher
 def test_decipher_method():
     gencipher = GeneticDecipher()
 
-    # Test decipher method with a known cipher text
+    # Test decipher method with
     cipher_text = (
         "Rbo rpktigo vcrb bwucja wj kloj hcjd, km sktpqo, cq rbwr loklgo "
         "vcgg cjqcqr kj skhcja wgkja wjd rpycja rk ltr rbcjaq cj cr."
     )
-    decoded_text = (
-        "The trouble with having an open mind, of course, is that people "
-        "will insist on coming along and trying to put things in it."
-    )
+
     deciphered_text = gencipher.decipher(cipher_text,
                                          max_iter=20,
                                          crossover_type="partially-mapped")
 
-    assert len(deciphered_text) == len(decoded_text)
-    assert (abs(gencipher.ngram.compute_fitness(deciphered_text)) >
-            abs(gencipher.ngram.compute_fitness(decoded_text)))
+    assert len(deciphered_text) == len(deciphered_text)
+    assert (gencipher.ngram.compute_fitness(deciphered_text) >=
+            gencipher.ngram.compute_fitness(cipher_text))
+
+
+def test_decipher_generator_method():
+    gencipher = GeneticDecipher()
+
+    # Test decipher_generator method
+    cipher_text = (
+        "Rbo rpktigo vcrb bwucja wj kloj hcjd, km sktpqo, cq rbwr loklgo "
+        "vcgg cjqcqr kj skhcja wgkja wjd rpycja rk ltr rbcjaq cj cr."
+    )
+
+    decipher_generator = gencipher.decipher_generator(
+        cipher_text,
+        max_iter=20,
+        crossover_type="partially-mapped"
+    )
+
+    for _, _, deciphered_text in decipher_generator:
+        assert len(deciphered_text) == len(cipher_text)
+        assert (gencipher.ngram.compute_fitness(deciphered_text) >=
+                gencipher.ngram.compute_fitness(cipher_text))
