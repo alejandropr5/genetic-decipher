163f5e6f3820f842e3239b7f282f219159dfc18e
alejandropr05 <alejandropr05@hotmail.com>
2024-01-24 23:37:22 -0500

    Add cipher text error test

--- tests/test_gencipher_model.py
+++ tests/test_gencipher_model.py
@@ -1,4 +1,5 @@
-from gencipher.model import GeneticDecipher
+import pytest
+from gencipher.model import GeneticDecipher, CipherTextLengthError
 
 
 def test_decipher_method():
@@ -38,3 +39,11 @@ def test_decipher_generator_method():
         assert len(deciphered_text) == len(cipher_text)
         assert (gencipher.ngram.compute_fitness(deciphered_text) >=
                 gencipher.ngram.compute_fitness(cipher_text))
+
+
+def test_cipher_text_error():
+    gencipher = GeneticDecipher(ngram_type="bigram")
+    cipher_text = "a"
+
+    with pytest.raises(CipherTextLengthError):
+        gencipher.decipher(cipher_text)
